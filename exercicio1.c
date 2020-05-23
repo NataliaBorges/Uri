b0d1a702de2bb3256d2813ea75acda364530ac9f
Natalia_Borges <60910920+NataliaBorges@users.noreply.github.com>
2020-05-23 16:31:39 -0300

    Create exercicio1.c

---
+++ exercicio1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int A, B, X;
+
+    printf("Entre o valor de A: ");
+    scanf("%d", &A);
+
+    printf("Entre o valor de B: ");
+    scanf("%d", &B);
+
+    X = A + B;
+
+    printf("O resultado equivale a: %d", X);
+    return 0;
+}
