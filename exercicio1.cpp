d8eb4d9f0190816eb4e2f98325ff60529be47fc2
Natalia_Borges <60910920+NataliaBorges@users.noreply.github.com>
2020-05-23 16:34:18 -0300

    Create exercicio1.cpp

---
+++ exercicio1.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+
+using namespace std ;
+
+int  main ()
+{
+    int A, B;
+    float X;
+
+    cin >> A;
+    cin >> B;
+
+    X = A + B;
+
+    cout << " X = " >> X;
+
+    return 0;
+}
