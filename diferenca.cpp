8c12ba137ec104096fece334d0e65595ee62c2cc
Natalia_Borges <60910920+NataliaBorges@users.noreply.github.com>
2020-05-23 22:11:46 -0300

    Create diferenca.cpp

---
+++ diferenca.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C, D, R;
+
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+
+    R = (A*B - C*D);
+
+    cout << "DIFERENCA = " << R << endl;
+
+    return 0;
+}
